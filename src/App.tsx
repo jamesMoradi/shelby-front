e9fdfc6ab7c52a8e2c450dcca44f3f43a7f29b72
james moradi <james.js.dev@outlook.com>
2024-04-10 16:48:52 +0430

    dibugged 01

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,18 @@ import Main from './layouts/Main'
 import Dashboard from './pages/Dashboard'
 import Welcome from './pages/Welcome'
 import BreakFasts from './pages/BreakFasts'
-import {store} from './redux/store'
-import { Provider } from 'react-redux'
 
 const App = () => {
   return (
       <BrowserRouter>
-      <Provider store={store}>
+
       <Routes>
-        <Route index element={<Welcome />}/>
+        <Route path='/' element={<Welcome />}/>
         <Route path='/admin' element={<Dashboard />}/>
         <Route path='/caffe-menu' element={<Main />}/>
         <Route path='/breakfast' element={<BreakFasts />}/>
       </Routes>
-      </Provider>
+
       </BrowserRouter>
   )
 }
